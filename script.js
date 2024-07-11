6c0705a7bb53681ef877b3016e10b30a1e3a6a60
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-07-11 13:42:51 -0300

    doing the style and adjusting JS

--- script.js
+++ script.js
@@ -2,174 +2,181 @@
 const formatador = (data) => {
   return {
     dia: {
-      numerico: dayjs(data).format('DD'),
+      numerico: dayjs(data).format("DD"),
       semana: {
-        curto: dayjs(data).format('ddd'),
-        longo: dayjs(data).format('dddd'),
-      }
+        curto: dayjs(data).format("ddd"),
+        longo: dayjs(data).format("dddd"),
+      },
     },
-    mes: dayjs(data).format('MMMM'),
-    hora: dayjs(data).format('HH:mm')
-  }
-}
+    mes: dayjs(data).format("MMMM"),
+    hora: dayjs(data).format("HH:mm"),
+  };
+};
 
 // object {}
 const atividade = {
   nome: "Almoço",
   data: new Date("2024-07-08 10:00"),
-  finalizada: true
-}
+  finalizada: true,
+};
 
 // lista, array, vetor []
 let atividades = [
   atividade,
   {
-    nome: 'Academia em grupo',
+    nome: "Academia em grupo",
     data: new Date("2024-07-09 12:00"),
-    finalizada: false
+    finalizada: false,
   },
   {
-    nome: 'Gamming session',
+    nome: "Gamming session",
     data: new Date("2024-07-09 16:00"),
-    finalizada: true
+    finalizada: true,
   },
-]
+];
 
 // atividades = []
 
 // arrow function
 const criarItemDeAtividade = (atividade) => {
-
   let input = `
   <input 
   onchange="concluirAtividade(event)"
   value="${atividade.data}"
   type="checkbox" 
-  `
+  `;
 
   if (atividade.finalizada) {
-    input += 'checked'
+    input += "checked";
   }
 
-  input += '>'
+  input += ">";
 
   const formatar = formatador(atividade.data);
 
   return `
-    <div>
+    <div class="card-bg">
       ${input}
-      <span>${atividade.nome}</span>
-      <time>${formatar.dia.semana.longo}, 
+
+      <div>
+        <img class="active" src="./assets/circle-check.svg" alt="">
+        <img class="inactive" src="./assets/circle-dashed.svg" alt="">
+
+        <span>${atividade.nome}</span>
+      </div>
+      
+
+      <time class="short">
+        ${formatar.dia.semana.curto}.
+        ${formatar.dia.numerico} <br>
+        ${formatar.hora}
+      </time>
+      
+      <time class="full">
+      ${formatar.dia.semana.longo}, 
       dia ${formatar.dia.numerico}
       de ${formatar.mes} 
       às ${formatar.hora}h </time>
     </div>
-    `
-}
-
+    `;
+};
 
 const atualizarListaDeAtividades = () => {
-  const section = document.querySelector('section')
-  section.innerHTML = ''
+  const section = document.querySelector("section");
+  section.innerHTML = "";
 
   // verificar se a minha lista está vazia
   if (atividades.length == 0) {
-    section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`
-    return
+    section.innerHTML = `<p>Nenhuma atividade cadastrada.</p>`;
+    return;
   }
 
   for (let atividade of atividades) {
-    section.innerHTML += criarItemDeAtividade(atividade)
+    section.innerHTML += criarItemDeAtividade(atividade);
   }
-}
+};
 
-atualizarListaDeAtividades()
+atualizarListaDeAtividades();
 
 const salvarAtividade = (event) => {
-  event.preventDefault()
-  const dadosDoFormulario = new FormData(event.target)
+  event.preventDefault();
+  const dadosDoFormulario = new FormData(event.target);
 
-  const nome = dadosDoFormulario.get('atividade')
-  const dia = dadosDoFormulario.get('dia')
-  const hora = dadosDoFormulario.get('hora')
-  const data = `${dia} ${hora}`
+  const nome = dadosDoFormulario.get("atividade");
+  const dia = dadosDoFormulario.get("dia");
+  const hora = dadosDoFormulario.get("hora");
+  const data = `${dia} ${hora}`;
 
   const novaAtividade = {
     nome,
     data,
-    finalizada: false
-  }
+    finalizada: false,
+  };
 
   const atividadeExiste = atividades.find((atividade) => {
-    return atividade.data == novaAtividade.data
-  })
+    return atividade.data == novaAtividade.data;
+  });
 
   if (atividadeExiste) {
-    return alert('Dia/Hora não disponível')
+    return alert("Dia/Hora não disponível");
   }
 
-  atividades = [novaAtividade, ...atividades]
-  atualizarListaDeAtividades()
-}
+  atividades = [novaAtividade, ...atividades];
+  atualizarListaDeAtividades();
+};
 
 const criarDiasSelecao = () => {
   const dias = [
-    '2024-02-28',
-    '2024-02-29',
-    '2024-03-01',
-    '2024-03-02',
-    '2024-03-03',
-  ]
+    "2024-02-28",
+    "2024-02-29",
+    "2024-03-01",
+    "2024-03-02",
+    "2024-03-03",
+  ];
 
-  let diasSelecao = ''
+  let diasSelecao = "";
 
   for (let dia of dias) {
-    const formatar = formatador(dia)
+    const formatar = formatador(dia);
     const diaFormatado = `
     ${formatar.dia.numerico} de 
     ${formatar.mes}
-    `
+    `;
     diasSelecao += `
     <option value="${dia}">${diaFormatado}</option>
-    `
+    `;
   }
 
-  document
-    .querySelector('select[name="dia"]')
-    .innerHTML = diasSelecao
-
-}
-criarDiasSelecao()
-
+  document.querySelector('select[name="dia"]').innerHTML = diasSelecao;
+};
+criarDiasSelecao();
 
 const criarHorasSelecao = () => {
-  let horasDisponiveis = ''
+  let horasDisponiveis = "";
 
   for (let i = 6; i < 23; i++) {
-    const hora = String(i).padStart(2, '0')
+    const hora = String(i).padStart(2, "0");
     horasDisponiveis += `
-    <option value="${hora}:00">${hora}:00</option>`
+    <option value="${hora}:00">${hora}:00</option>`;
     horasDisponiveis += `
-    <option value="${hora}:30">${hora}:30</option>`
+    <option value="${hora}:30">${hora}:30</option>`;
   }
 
-  document
-    .querySelector('select[name="hora"]')
-    .innerHTML = horasDisponiveis
-}
-criarHorasSelecao()
+  document.querySelector('select[name="hora"]').innerHTML = horasDisponiveis;
+};
+criarHorasSelecao();
 
 const concluirAtividade = (event) => {
-  const input = event.target
-  const dataDesteInput = input.value
+  const input = event.target;
+  const dataDesteInput = input.value;
 
   const atividade = atividades.find((atividade) => {
-    return atividade.data == dataDesteInput
-  })
+    return atividade.data == dataDesteInput;
+  });
 
-  if(!atividade) {
-    return
+  if (!atividade) {
+    return;
   }
 
-  atividade.finalizada = !atividade.finalizada
-}
+  atividade.finalizada = !atividade.finalizada;
+};
